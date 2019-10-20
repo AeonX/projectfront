0297a2aac18acefa207a878859c13d8eed5e00c3
Aeonify <kedaren.dorasami@gmail.com>
2019-10-20 11:53:28 +0400

    added theme switcher and changed app colors

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { UserService } from 'src/app/service/user.service';
+import Darkmode from 'darkmode-js';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,30 @@ import { UserService } from 'src/app/service/user.service';
 })
 export class HeaderComponent implements OnInit {
 
+  options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#2f3640',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+
+  darkmode = new Darkmode(this.options);
+
   constructor(private authenticationService: AuthenticationService, private userService: UserService) { }
 
   ngOnInit() {
     this.userService.isUserLoggedIn();
+  }
+
+  changeTheme() {
+    this.darkmode.toggle();
   }
 
 }
